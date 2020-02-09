857ff66c3797e837de81ad35ee1df4b0215dd4a1
gadda <gil.adda@cyberark.com>
2020-02-09 13:30:09 +0000

    created dynamodb tables for connections and records

--- ws_cpm_guildathon/ws_cpm_guildathon_stack.py
+++ ws_cpm_guildathon/ws_cpm_guildathon_stack.py
@@ -1,9 +1,20 @@
-from aws_cdk import core
-
+from aws_cdk import core, aws_dynamodb as dynamodb
 
 class WsCpmGuildathonStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        self.connections_table = dynamodb.Table(self, f"WSPlaygroundConnections",
+                                             partition_key=dynamodb.Attribute(
+                                                 name="connection_id",
+                                                 type=dynamodb.AttributeType.STRING),
+                                             billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+                                             removal_policy=core.RemovalPolicy.DESTROY)
+
+        self.connections_table = dynamodb.Table(self, f"WSPlaygroundRecords",
+                                             partition_key=dynamodb.Attribute(
+                                                 name="connection_id",
+                                                 type=dynamodb.AttributeType.STRING),
+                                             billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+                                             removal_policy=core.RemovalPolicy.DESTROY)
